5336d85b586950d9770f57ba404a4c42b6f0a6a9
Luca Del Puppo <luca@delpuppo.net>
2023-12-15 01:10:29 +0100

    Day 09

---
+++ src/day09/index.ts
@@ -0,0 +1,71 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => 
+  rawInput
+    .trim()
+    .split('\n')
+    .map(line => line
+                .split(' ')
+                .map(Number));
+
+const getNext = (sequence: number[]) => {
+  const diffSeqs: number[][] = [];
+  let currentSeq: number[] = sequence;
+  while (currentSeq.some(Boolean)) {
+    diffSeqs.push(currentSeq);
+    currentSeq = Array.from(
+      { length: currentSeq.length - 1},
+      (_, index) => currentSeq[index + 1] - currentSeq[index]
+    );
+  }
+
+  return diffSeqs.reduceRight((sum, seq) => sum + seq[seq.length -1], 0);
+};
+
+const part1 = (rawInput: string) => {
+  const sequences = parseInput(rawInput);
+
+  return sequences.map(getNext).reduce((sum, value) => sum + value);
+};
+
+const getPrevious = (sequence: number[]) => {
+  const diffSeqs: number[][] = [];
+  let currentSeq = sequence;
+  while (currentSeq.some(Boolean)) {
+    diffSeqs.push(currentSeq);
+    currentSeq = Array.from(
+      { length: currentSeq.length - 1},
+      (_, index) => currentSeq[index + 1] - currentSeq[index]
+    );
+  }
+  return diffSeqs.reduceRight((sum, seq) => seq[0] - sum, 0);
+};
+
+const part2 = (rawInput: string) => {
+  const sequences = parseInput(rawInput);
+
+  return sequences.map(getPrevious).reduce((sum, value) => sum + value);
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
