072c96c9a58cfd69b4168c15c454cc5d43d4ae3b
Luca Del Puppo <luca@delpuppo.net>
2023-12-16 09:45:08 +0100

    Complete Day 14

--- src/day14/index.ts
+++ src/day14/index.ts
@@ -1,92 +1,128 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput.split("\n");
+const parseInput = (rawInput: string) =>
+  rawInput
+    .trim()
+    .split("\n")
+    .map((x) => x.split(""));
 
-function rollToNorth(grid: string[]) {
-  for (let y = 0; y < grid.length; y++) {
-      for (let x = 0; x < grid[0].length; x++) {
-          if (grid[y][x] != 'O') continue;
-          for (let i = 0; i < y; i++) {
-              if ('#O'.includes(grid[y - i - 1][x])) break;
-              grid[y - i] = grid[y - i].slice(0, x) + '.' + grid[y - i].slice(x + 1);
-              grid[y - i - 1] = grid[y - i - 1].slice(0, x) + 'O' + grid[y - i - 1].slice(x + 1);
-          }
+const CYCLES = 1_000_000_000;
+
+function rebuildGrid(rawGrid: string) {
+  return rawGrid
+    .trim()
+    .split("\n")
+    .map((line) => line.split(""));
+}
+
+function rollToNorth(grid: string[][]) {
+  for (let row = 0; row < grid.length; row++) {
+    for (let column = 0; column < grid[row].length; column++) {
+      if (grid[row][column] !== "O") continue;
+      let newRow = row - 1;
+      while (newRow >= 0 && grid[newRow][column] === ".") newRow--;
+      if (newRow < row - 1) {
+        grid[newRow + 1][column] = "O";
+        grid[row][column] = ".";
       }
+    }
   }
   return grid;
 }
 
-function rollToWest(grid: string[]) {
-  for (let y = 0; y < grid.length; y++) {
-      for (let x = 0; x < grid[0].length; x++) {
-          if (grid[y][x] != 'O') continue;
-          for (let i = 0; i < x; i++) {
-              if ('#O'.includes(grid[y][x - i - 1])) break;
-              grid[y] = grid[y].slice(0, x - i - 1) + 'O.' + grid[y].slice(x - i + 1);
-          }
+function rollToWest(grid: string[][]): string[][] {
+  for (let row = 0; row < grid.length; row++) {
+    const line = grid[row];
+    for (let column = 0; column < line.length; column++) {
+      if (line[column] !== "O") continue;
+      let newColumn = column - 1;
+      while (newColumn >= 0 && line[newColumn] === ".") newColumn--;
+      if (newColumn < column - 1) {
+        line[newColumn + 1] = "O";
+        line[column] = ".";
       }
+    }
   }
   return grid;
 }
 
-function rollRocksEast(grid: string[]) {
-  for (let y = 0; y < grid.length; y++) {
-      for (let x = grid[0].length; x > 0; x--) {
-          if (grid[y][x - 1] != 'O') continue;
-          for (let i = 0; i < grid[0].length - x; i++) {
-              if ('#O'.includes(grid[y][x + i])) break;
-              grid[y] = grid[y].slice(0, x + i - 1) + '.O' + grid[y].slice(x + i + 1);
-          }
+function rollToSouth(grid: string[][]): string[][] {
+  for (let row = grid.length - 1; row >= 0; row--) {
+    const line = grid[row];
+    for (let column = 0; column < line.length; column++) {
+      if (line[column] !== "O") continue;
+
+      let newRow = row + 1;
+      while (newRow < grid.length && grid[newRow][column] === ".") newRow++;
+      if (newRow > row + 1) {
+        grid[newRow - 1][column] = "O";
+        grid[row][column] = ".";
       }
+    }
   }
   return grid;
 }
 
-function rollRocksSouth(grid: string[]) {
-  for (let y = grid.length; y > 0; y--) {
-      for (let x = 0; x < grid[0].length; x++) {
-          if (grid[y - 1][x] != 'O') continue;
-          for (let i = 0; i < grid[0].length - y; i++) {
-              if ('#O'.includes(grid[y + i][x])) break;
-              grid[y + i - 1] = grid[y + i - 1].slice(0, x) + '.' + grid[y + i - 1].slice(x + 1);
-              grid[y + i] = grid[y + i].slice(0, x) + 'O' + grid[y + i].slice(x + 1);
-          }
+function rollToEast(grid: string[][]): string[][] {
+  for (let row = 0; row < grid.length; row++) {
+    const line = grid[row];
+
+    for (let column = line.length - 1; column >= 0; column--) {
+      if (line[column] !== "O") continue;
+      let newColumn = column + 1;
+      while (newColumn < line.length && line[newColumn] === ".") newColumn++;
+      if (newColumn > column + 1) {
+        line[newColumn - 1] = "O";
+        line[column] = ".";
       }
+    }
   }
   return grid;
 }
 
-function calculateScore(pattern: string[]): number {
-  let lineScore = pattern.length;
+function roll(grid: string[][]): string[][] {
+  return rollToEast(rollToSouth(rollToWest(rollToNorth(grid))));
+}
+
+function calculateScore(grid: string[][]): number {
+  let lineScoreValue = grid.length;
   let score = 0;
-  for (let row of pattern) {
-    const blocks = row.split('').filter(c => c === 'O').length;
-    score += blocks * lineScore--;
+  for (let row of grid) {
+    const blocks = row.filter((c) => c === "O").length;
+    const lineScore = blocks * lineScoreValue;
+    score += lineScore;
+    lineScoreValue--;
   }
-
   return score;
+}
+
+function serialize(grid: string[][]) {
+  return grid.map((line) => line.join("")).join("\n");
 }
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return calculateScore(rollToNorth(input))
+  return calculateScore(rollToNorth(input));
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-  let grid = input;
-  for (let i = 0; i < 1_000_000_000; i++) {
-    if (i % 1_000_000 === 0) console.log(i);
-    grid = rollToNorth(grid);
-    grid = rollToWest(grid);
-    grid = rollRocksSouth(grid);
-    grid = rollRocksEast(grid);
+  let grid = parseInput(rawInput);
+  const history: string[] = [];
+  let gridIndex = -1;
+  for (let count = 0; count < CYCLES; count++) {
+    const rawGrid = serialize(roll(grid));
+    gridIndex = history.indexOf(rawGrid);
+    // We've seen this grid before, we're in a cycle.
+    if (gridIndex >= 0) break;
+    history.push(rawGrid);
   }
+  const gridCycleLength = history.length - gridIndex;
+  // So this is the billionth state. Trust me bro.
+  const lastGridOnCycle =
+    history[gridIndex + ((CYCLES - gridIndex) % gridCycleLength) - 1];
 
-  console.log(grid.join('\n'));
-
-  return calculateScore(grid)
+  return calculateScore(rebuildGrid(lastGridOnCycle));
 };
 
 run({
